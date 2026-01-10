f19432a0b963814fa09242ac556fd07358c89b61
Servando Hernandez <s_musicpia@yahoo.com.mx>
2026-01-10 13:33:54 -0600

    Add Rating column

---
+++ MvcMovie/Migrations/20260110193227_AddRating.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldPrecision: 18,
+                oldScale: 4);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 4);
+        }
+    }
+}
--- MvcMovie/SeedData/SeederDb.cs
+++ MvcMovie/SeedData/SeederDb.cs
@@ -24,119 +24,136 @@ public class SeederDb
                 Title = "When Harry Met Sally",
                 ReleaseDate = DateTime.Parse("1989-2-12"),
                 Genre = "Romantic Comedy",
-                Price = 7.99M
+                Price = 7.99M,
+                Rating = "R"
             },
             new Movie
             {
                 Title = "Ghostbusters ",
                 ReleaseDate = DateTime.Parse("1984-3-13"),
                 Genre = "Comedy",
-                Price = 8.99M
+                Price = 8.99M,
+                Rating = "PG"
             },
             new Movie
             {
                 Title = "Ghostbusters 2",
                 ReleaseDate = DateTime.Parse("1986-2-23"),
                 Genre = "Comedy",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "PG"
             },
             new Movie
             {
                 Title = "Rio Bravo",
                 ReleaseDate = DateTime.Parse("1959-4-15"),
                 Genre = "Western",
-                Price = 3.99M
+                Price = 3.99M,
+                Rating = "NR"
             },
             new Movie
             {
                 Title = "Inception",
                 ReleaseDate = DateTime.Parse("2010-7-16"),
                 Genre = "Science Fiction",
-                Price = 10.99M
+                Price = 10.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "The Dark Knight",
                 ReleaseDate = DateTime.Parse("2008-7-18"),
                 Genre = "Action",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "Interstellar",
                 ReleaseDate = DateTime.Parse("2014-11-7"),
                 Genre = "Science Fiction",
-                Price = 12.99M
+                Price = 12.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "The Matrix",
                 ReleaseDate = DateTime.Parse("1999-3-31"),
                 Genre = "Science Fiction",
-                Price = 8.99M
+                Price = 8.99M,
+                Rating = "R"
             },
             new Movie
             {
                 Title = "Pulp Fiction",
                 ReleaseDate = DateTime.Parse("1994-10-14"),
                 Genre = "Crime",
-                Price = 7.99M
+                Price = 7.99M,
+                Rating = "R"
             },
             new Movie
             {
                 Title = "The Shawshank Redemption",
                 ReleaseDate = DateTime.Parse("1994-9-23"),
                 Genre = "Drama",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "R"
             },
             new Movie
             {
                 Title = "The Godfather",
                 ReleaseDate = DateTime.Parse("1972-3-24"),
                 Genre = "Crime",
-                Price = 11.99M
+                Price = 11.99M,
+                Rating = "R"
             },
             new Movie
             {
                 Title = "Forrest Gump",
                 ReleaseDate = DateTime.Parse("1994-7-6"),
                 Genre = "Drama",
-                Price = 8.99M
+                Price = 8.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "The Lion King",
                 ReleaseDate = DateTime.Parse("1994-6-15"),
                 Genre = "Animation",
-                Price = 7.99M
+                Price = 7.99M,
+                Rating = "G"
             },
             new Movie
             {
                 Title = "Titanic",
                 ReleaseDate = DateTime.Parse("1997-12-19"),
                 Genre = "Romance",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "Avatar",
                 ReleaseDate = DateTime.Parse("2009-12-18"),
                 Genre = "Science Fiction",
-                Price = 10.99M
+                Price = 10.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "Avengers: Endgame",
                 ReleaseDate = DateTime.Parse("2019-4-26"),
                 Genre = "Action",
-                Price = 12.99M
+                Price = 12.99M,
+                Rating = "PG-13"
             },
             new Movie
             {
                 Title = "Joker",
                 ReleaseDate = DateTime.Parse("2019-10-4"),
                 Genre = "Crime",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "R"
             }
         );
         context.SaveChanges();
